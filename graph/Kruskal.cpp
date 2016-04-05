b03b07e3b73a6bbcb42d5475043444f2335e1d7f
xiaoyuwei <xyw1105@126.com>
2016-04-05 19:12:26 +0800

    v0.1 not tested

---
+++ graph/Kruskal.cpp
@@ -0,0 +1,99 @@
+/**
+ *	Algorithm:  Kruskal - Minimum Spanning tree
+ *	
+ * 	Author: 	Alpha Xiao
+ * 	Date  :		2016.4.5
+ * 	Language:  	C++11
+ *
+ * 	Description:  
+ * 				  
+ *
+ *  TODO:
+ *  	test 
+ *   	support more parameter type
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define INTMAX 0x7fffffff
+
+struct edge
+{
+	int from, to;
+	int w;
+	static bool compare(const edge &lhs, const edge &rhs) {
+		return lhs.w > rhs.w ? 1 : 0;
+	}
+};
+
+class UnionSet
+{
+public:
+	/**
+	 * Constructor.
+	 * The initialize value of each element is -1,
+	 * which means they are the only element in 
+	 * their set.
+	 */
+	UnionSet(int size)
+		:vec(size+1, -1)
+	{}
+
+	void union_ele(int x,int y)
+	{
+		int xf = findFather(x);
+		int yf = findFather(y);
+		if( xf == yf )
+			return ;
+		//xf size is smaller, swap them, then xf is bigger
+		if(vec[xf] > vec[yf])
+			std::swap(xf,yf);
+		//then union yf to xf
+		vec[xf] = vec[xf] + vec[yf];
+		vec[yf] = xf;
+	}
+
+	bool isInSameSet(int x,int y)
+	{
+		return findFather(x) == findFather(y);
+	}
+
+private:
+
+	int findFather(int x)
+	{
+		if(vec[x] < 0)
+			return x;
+		else 
+			/* path compression */
+			return vec[x] = findFather(vec[x]);
+	}
+
+	std::vector<int> vec;
+};
+
+/**
+ * 
+ */
+std::vector<edge> Kruskal(std::vector<edge> &G, int v) {
+
+	std::sort(G.begin(), G.end(), edge::compare);
+	UnionSet U(v);
+	std::vector<edge> ans;
+	int k = -1;
+	while(ans.size() != v-1) {
+		++k;
+		if( U.isInSameSet(G[k].from, G[k].to) )
+			continue;
+		ans.push_back(G[k]);
+		U.union_ele(G[k].from, G[k].to);
+	}
+	return ans;
+}
+
+int main()
+{
+
+}
---
+++ graph/Prim.cpp
@@ -0,0 +1,92 @@
+/**
+ *	Algorithm:  Prim - Minimum Spanning tree
+ *	
+ * 	Author: 	Alpha Xiao
+ * 	Date  :		2016.4.5
+ * 	Language:  	C++11
+ *
+ * 	Description:  
+ * 				  
+ *
+ *  TODO:
+ *  	test 
+ *  	using heap to reduce time complex
+ */
+
+#include <iostream>
+#include <vector>
+
+
+class adjacent_list{
+public:
+
+	adjacent_list(int n)
+		:pos(n, -1), flag(0), size(n)
+	{}
+
+	/**
+	 * add edge x->y with weight = tw
+	 */
+	void add_edge(int x, int y, int tw = 0)
+	{
+		pre.push_back(pos[x]);
+		v.push_back(y);
+		w.push_back(tw);
+		pos[x] = flag++;
+	}
+
+	int size;
+	int flag;				//size of total list, = pre.size() = v.size() = w.size()
+	std::vector<int> pos;	//start position of each node. space O(n)
+	std::vector<int> pre;	//previous edge, space O(m), value:-1 mean no previous edge
+	std::vector<int> v;		//correpsoned node, space O(m)
+	std::vector<int> w;		//weight of the edge, space O(m)
+};
+
+#define INTMAX 0x7fffffff
+/**
+ * [Prim description]
+ * @param  G graph G(V,E)
+ * @return   a minimum spanning tree built on G
+ */
+adjacent_list Prim(adjacent_list &G) {
+
+	std::vector<int> dist(G.size, INTMAX);
+	std::vector<int> parent(G.size, 0);		/* record the edge of MST */
+	std::vector<bool> S(G.size, false);
+	dist[0] = 0;
+
+	for(int i = 0; i < G.size; ++i) {
+		int min = INTMAX;
+		int next;
+		for(int j = 0; j < G.size; ++j) //pick minimum vertex into tree
+		if(min > dist[j] && !S[j]) {
+			next = j;
+			min = dist[j];
+		}
+
+		S[next] = true;
+
+		/* update dist array using the minimum vertex */
+		for(int j = G.pos[next]; j != -1; j = G.pre[j]) {
+			int node = G.v[j];
+			int value = G.w[j];
+			if( dist[node] > value + dist[next] ) {
+				dist[node] = value + dist[next];
+				parent[node] = next;
+			}
+		}
+	}
+
+	adjacent_list ans(G.size);
+	for(int i = 1; i < G.size; ++i){
+		ans.add_edge(i, parent[i]);
+		ans.add_edge(parent[i], i);
+	}
+}
+
+
+int main()
+{
+
+}
