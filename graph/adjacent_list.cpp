7d058304a8771234c22dae8783a94d8690fff1bb
AlphaShawn <xiaoyuwei_yun@qq.com>
2016-05-10 16:52:20 +0800

    add assignment operator to adjacent_list

--- graph/adjacent_list.cpp
+++ graph/adjacent_list.cpp
@@ -1,4 +1,17 @@
+/**
+ *  Adjacent_list
+ *  Author: Alpha Xiao
+ *
+ * 	
+ *  update:
+ *  	5.10  alpha xiao
+ *  		  1. add assignment operator
+ *  		  2. add printGraph() method
+ */
+
 #include <iostream>
+#include <vector>
+#include <stdio.h>
 
 
 /**
@@ -22,9 +35,47 @@ public:
 		pos[x] = flag++;
 	}
 
+	adjacent_list& operator=(const adjacent_list &rhs) {
+		flag = rhs.flag;
+		pos = rhs.pos;
+		pre = rhs.pos;
+		v = rhs.pos;
+		w = rhs.w;
+		return *this;
+	}
+
+	/**
+	 * print the graph
+	 */
+	void printGraph() {
+		for(int i = 0; i < pos.size(); ++i) {
+			for(int j = pos[i]; j != -1; j = pre[j]) {
+				printf("from:%d-to:%d- weight:%d\n", i, v[j], w[j]);
+			}
+		}
+	}
+
 	int flag;				//size of total list, = pre.size() = v.size() = w.size()
 	std::vector<int> pos;	//start position of each node. space O(n)
 	std::vector<int> pre;	//previous edge, space O(m), value:-1 mean no previous edge
 	std::vector<int> v;		//correpsoned node, space O(m)
 	std::vector<int> w;		//weight of the edge, space O(m)
 };
+
+
+/**
+ * test
+ */
+int main() {
+	adjacent_list G(6);
+	G.add_edge(1,2,3);
+	G.add_edge(2,3,4);
+	adjacent_list tG = G;
+
+	tG.add_edge(4,5,4);
+	G.printGraph();
+	printf("\n");
+	tG.printGraph();
+
+	return 0;
+}
